80ca0b652f0ba58f0e5abd1eba8611fe7bb072d4
ChemiAtlow <chemiatlow@gmail.com>
2023-01-26 19:48:02 +0200

    lint, fix `prettierignore` & fix `tsconfig.json`

--- example/src/main.ts
+++ example/src/main.ts
@@ -8,11 +8,12 @@ const baseSelector = iconBaseSelector.replace('.', '');
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>vite-svg-2-webfont</h1>
-    <div id="icons">${icons.map(iconName => {
-          const className = `${baseSelector} ${iconClassPrefix}${iconName}`;
-          return `<div class="card"><i class="${className}"></i></div>`;
-        }).join('\n')
-    }</div>
+    <div id="icons">${icons
+        .map(iconName => {
+            const className = `${baseSelector} ${iconClassPrefix}${iconName}`;
+            return `<div class="card"><i class="${className}"></i></div>`;
+        })
+        .join('\n')}</div>
     <a class="read-the-docs" href="https://github.com/ChemiAtlow/vite-svg-2-webfont#readme">
       Read the docs
     </a>
