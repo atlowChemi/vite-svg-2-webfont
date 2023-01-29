1f539c92750992f4ba595fe1b4392e3a9874e84b
ChemiAtlow <chemiatlow@gmail.com>
2023-01-30 00:04:14 +0200

    prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    coverage: {
-      provider: 'istanbul',
+    test: {
+        coverage: {
+            provider: 'istanbul',
+        },
     },
-  },
 });
