c622abeeb657f63fef9e609b2a0cf04376f3d942
atlowChemi <chemi@atlow.co.il>
2024-03-02 23:52:29 +0200

    return support for vite 4

--- src/types/publicApi.ts
+++ src/types/publicApi.ts
@@ -1,5 +1,15 @@
+import type { Plugin } from 'vite';
 import type { GeneratedWebfont } from './generatedWebfont';
 
+
+/**
+ * Compatibility type for Vite plugins.
+ * 
+ * Generic API support was added in vite 5+, this type is needed to also support older versions.
+ */
+export interface CompatiblePlugin<T> extends Plugin {
+    api?: T;
+}
 export interface PublicApi {
     getGeneratedWebfonts(): GeneratedWebfont[];
 }
