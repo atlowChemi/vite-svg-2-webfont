2ecbbbc8ebf09aefc1d591f1dc08ea216c37e761
Chemi Atlow <chemi@atlow.co.il>
2025-01-29 22:34:27 +0200

    Support vite v6 (#43)

--- example/src/webfont/icons.ts
+++ example/src/webfont/icons.ts
@@ -1,8 +1,8 @@
 export const iconBaseSelector = '.exIcon' as const;
 export const iconClassPrefix = 'icon-' as const;
 export const icons = [
-    'add',
     'calendar',
+    'add',
 ] as const;
 
 export type IconOption = typeof icons[number];
--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig(options => ({
     entry: ['src/index.ts'],
-    target: 'node16',
+    target: 'node18.0',
     dts: true,
     format: ['esm', 'cjs'],
     clean: !options.watch,
