f8f18322359ec059d878cb569f1cf9251a0c4225
atlowChemi <chemi@atlow.co.il>
2026-08-30 19:32:58 +0300

    test(webfont-generator): cover NAPI input loading

--- packages/webfont-generator/src/input/files/tests.rs
+++ packages/webfont-generator/src/input/files/tests.rs
@@ -1,3 +1,5 @@
+#[cfg(feature = "napi")]
+use super::load_svg_files_napi;
 use super::{default_glyph_name_from_path, glyph_name_from_path};
 
 #[test]
@@ -17,4 +19,21 @@ fn errors_when_glyph_name_cannot_be_derived() {
             .to_string()
             .contains("Unable to derive glyph name from '/tmp/icons/..'.")
     );
+}
+
+#[cfg(feature = "napi")]
+#[tokio::test]
+async fn napi_loader_uses_default_glyph_names() {
+    let path = std::env::temp_dir().join(format!("webfont-input-{}-icon.svg", std::process::id()));
+    std::fs::write(&path, "<svg />").unwrap();
+    let paths = vec![path.to_string_lossy().into_owned()];
+
+    let source_files = load_svg_files_napi(&paths, None).await.unwrap();
+
+    assert_eq!(
+        source_files[0].glyph_name,
+        format!("webfont-input-{}-icon", std::process::id())
+    );
+    assert_eq!(&*source_files[0].contents, "<svg />");
+    std::fs::remove_file(path).unwrap();
 }
